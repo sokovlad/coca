47d77c5462d7c4c2d705a0d7d3623d8dfaf85798
Vlad <vampir2204@gmail.com>
2024-08-14 19:40:57 +0300

    Created pricing: hero section, fixed header-scroll

--- src/js/components/header-scroll.js
+++ src/js/components/header-scroll.js
@@ -1,16 +1,25 @@
 export const scrollHeader = () => {
   const header = document.querySelector('.header');
   const hero = document.querySelector('.hero');
+
   let lastScrollTop = 0;
   document.addEventListener('scroll', () => {
     const scroll2 = document.documentElement.scrollTop;
     if (scroll2 > 0 && scroll2 < 100) {
       header.classList.add('header--fixed');
-      hero.style.paddingTop = 83 + 117 + 'px';
+      if (hero.classList.contains('hero--home')) {
+        hero.style.paddingTop = `calc(var(--pt-hero-home) + var(--header-height))`;
+      } else if (hero.classList.contains('hero--pricing')) {
+        hero.style.paddingTop = `calc(var(--pt-hero-pricing) + var(--header-height))`;
+      }
     }
     if (scroll2 === 0) {
       header.classList.remove('header--fixed');
-      hero.style.paddingTop = 83 + 'px';
+      if (hero.classList.contains('hero--home')) {
+        hero.style.paddingTop = 'var(--pt-hero-home)';
+      } else if (hero.classList.contains('hero--pricing')) {
+        hero.style.paddingTop = 'var(--pt-hero-pricing)';
+      }
       header.classList.add('header--show');
     }
     if (lastScrollTop < scroll2 && scroll2 > 100) {
---
+++ src/js/pricing.js
@@ -0,0 +1,13 @@
+import '../assets/scss/pricing.scss';
+
+import { useTheme } from './components/theme';
+import { useBurger } from './components/burger';
+import { scrollHeader } from './components/header-scroll';
+import { useFooterAccordion } from './components/footer-accordion';
+import { usePriceSwitcher } from './components/pricing/priceSwitcher';
+
+useTheme();
+useBurger();
+scrollHeader();
+useFooterAccordion();
+usePriceSwitcher();
